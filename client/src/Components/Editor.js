cd9817fe4be37600f78393eb9a225a9c292ff957
Himanshu <hv65616@gmail.com>
2024-05-16 09:18:56 +0530

    editor page create

--- client/src/Components/Editor.js
+++ client/src/Components/Editor.js
@@ -1,9 +1,25 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react";
+import Codemirror from "codemirror";
+import "codemirror/mode/javascript/javascript";
+import "codemirror/theme/dracula.css";
+import "codemirror/addon/edit/closetag";
+import "codemirror/addon/edit/closebrackets";
+import "codemirror/lib/codemirror.css";
 const Editor = () => {
-  return (
-    <div>Editor</div>
-  )
-}
+  useEffect(() => {
+    async function init() {
+      console.log("Hello");
+      Codemirror.fromTextArea(document.getElementById("realtimeEditor"), {
+        mode: { name: "javascript", json: true },
+        theme: "dracula",
+        autoCloseTags: true,
+        autoCloseBrackets: true,
+        lineNumbers: true,
+      });
+    }
+    init();
+  }, []);
+  return <textarea name="" id="realtimeEditor"></textarea>;
+};
 
-export default Editor
+export default Editor;
