3587d55a777b565ad41d265839d4cc786ddb9833
Himanshu <hv65616@gmail.com>
2024-05-16 08:23:30 +0530

    editor pade aside created

--- client/src/Pages/EditorPage.js
+++ client/src/Pages/EditorPage.js
@@ -1,9 +1,44 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import Client from "../Components/Client";
+import Editor from "../Components/Editor";
 const EditorPage = () => {
+  const [clients, setClients] = useState([
+    { socketId: 1, username: "Himanshu" },
+    { socketId: 2, username: "Rakesh" },
+    {
+      socketId: 3,
+      username: "Himanshu",
+    },
+  ]);
   return (
-    <div>EditorPage</div>
-  )
-}
+    <>
+      <div className="mainWrap">
+        <div className="aside">
+          <div className="asideInner">
+            <div className="logo">
+              <img className="logoImage" src="/code-sync.png" alt="Logo" />
+            </div>
+            <h3>Connected</h3>
+            <div className="clientsList">
+              {clients.map((client) => {
+                return (
+                  <Client
+                    key={client.socketId}
+                    username={client.username}
+                  ></Client>
+                );
+              })}
+            </div>
+          </div>
+          <button className="btn copyBtn"> Copy Room ID</button>
+          <button className="btn leaveBtn">Leave</button>
+        </div>
+          <div className="editorWrap">
+            <Editor></Editor>
+          </div>
+      </div>
+    </>
+  );
+};
 
-export default EditorPage
+export default EditorPage;
