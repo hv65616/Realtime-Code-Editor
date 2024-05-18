8cb5223fe1f933646519f64c97b2f0b6f0746e97
Himanshu <hv65616@gmail.com>
2024-05-18 23:16:25 +0530

    socket disconnecting

--- server/index.js
+++ server/index.js
@@ -34,6 +34,17 @@ io.on("connection", (socket) => {
       });
     });
   });
+  socket.on("disconnecting", () => {
+    const rooms = [...socket.rooms];
+    rooms.forEach((roomId) => {
+      socket.in(roomId).emit(ACTIONS.DISCONNECTED, {
+        socketId: socket.id,
+        username: userSocketMap[socket.id],
+      });
+    });
+    delete userSocketMap[socket.id];
+    socket.leave();
+  });
 });
 
 server.listen(5000, (req, res) => {
